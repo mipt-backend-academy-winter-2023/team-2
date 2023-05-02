7bc21a7a518b05a0d7b60737b1b53f20ac6b5e08
EgorovGA <egorov.ga@minc.tech>
2023-05-02 14:15:27 +0300

    first commit - work in localhost; need real routings and multiple servers running

--- auth/src/main/scala/auth/AuthMain.scala
+++ auth/src/main/scala/auth/AuthMain.scala
@@ -6,11 +6,22 @@ import zio.http.Server
 import zio.{Scope, ZIO, ZIOAppArgs, ZIOAppDefault, http}
 
 object AuthMain extends ZIOAppDefault {
-  override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = {
+  /*override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = {
     zio.http.Server.serve(HttpRoutes.app)
       .provide(
         Server.live,
         ServiceConfig.live,
       )
+  }*/
+
+  override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = {
+    for {
+      _ <- ZIO.logInfo("Start AuthMain")
+      _ <- zio.http.Server.serve(HttpRoutes.app)
+        .provide(
+          Server.live,
+          ServiceConfig.live,
+        )
+    } yield ()
   }
 }
--- auth/src/main/scala/auth/api/HttpRoutes.scala
+++ auth/src/main/scala/auth/api/HttpRoutes.scala
@@ -1,7 +1,25 @@
 package auth.api
 
+import zio.ZIO
 import zio.http._
+import zio.http.model.{Method, Status}
 
 object HttpRoutes {
-  val app: HttpApp[Any, Response] = ???
+
+  val app: HttpApp[Any, Response] = Http.collectZIO[Request] {
+    case Method.GET -> !! / "route" / "find" => ZIO.succeed(Response.text("Hello2233"))
+    case req@Method.POST -> !! / "greeting" / "by" =>
+      val response =
+        for {
+          name <- ZIO.fromOption(
+            req
+              .url
+              .queryParams
+              .get("name")
+              .flatMap(_.headOption)
+          ).tapError(_ => ZIO.logError("not provide id"))
+        } yield Response.text(s"Hello $name")
+      response.orElseFail(Response.status(Status.BadRequest))
+  }
+
 }
