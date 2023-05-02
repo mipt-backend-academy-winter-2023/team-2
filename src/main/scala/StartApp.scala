ab21a53a58d3db523b523b18af082fbc3871859c
EgorovGA <egorov.ga@minc.tech>
2023-05-02 19:19:00 +0300

    docker script

--- src/main/scala/StartApp.scala
+++ src/main/scala/StartApp.scala
@@ -6,8 +6,8 @@ import zio.{Scope, ZIO, ZIOAppArgs, ZIOAppDefault}
 object StartApp extends ZIOAppDefault {
   override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] =
     for {
-      _ <- RoutingMain.run
-      //_ <- AuthMain.run
+      //_ <- RoutingMain.run
+      _ <- AuthMain.run
       //_ <- HelperMain.run
     } yield ()
 }
