14e8d7867c57cd72a3c9826bda7c5388e391af71
EgorovGA <egorov.ga@minc.tech>
2023-05-02 18:02:10 +0300

    routes done

--- routing/src/main/scala/routing/api/HttpRoutes.scala
+++ routing/src/main/scala/routing/api/HttpRoutes.scala
@@ -20,55 +20,11 @@ object PointsResp {
 }
 
 object HttpRoutes {
-
   val app: HttpApp[Any, Response] = Http.collectZIO[Request] {
     case req@Method.GET -> !! / "route" / "find" if (req.url.queryParams.nonEmpty) => {
-      //val points = req.url.queryParams("ids").flatMap(id => Point(id, "name" + id))
       val points = req.url.queryParams("ids").foldLeft(List[Point]())((acc, next) => Point(Integer.valueOf(next), "name" + next) :: acc)
       ZIO.succeed(Response.json(PointsResp(points).toJson))
-      //ZIO.succeed(Response.text(s"Hello ${req.url.queryParams("ids").mkString(" and ")}!"))
     }
   }
 
 }
-
-
-
-
-/*case class UserReq(name: String, password: String)
-
-object UserReq {
-  implicit val encoder: JsonEncoder[UserReq] = DeriveJsonEncoder.gen[UserReq]
-  implicit val decoder: JsonDecoder[UserReq] = DeriveJsonDecoder.gen[UserReq]
-}
-
-case class TokenReq(token: String)
-
-object TokenReq {
-  implicit val encoder: JsonEncoder[TokenReq] = DeriveJsonEncoder.gen[TokenReq]
-  implicit val decoder: JsonDecoder[TokenReq] = DeriveJsonDecoder.gen[TokenReq]
-}
-
-object HttpRoutes {
-
-  val app: HttpApp[Any, Response] = Http.collectZIO[Request] {
-    case req@Method.POST -> !! / "auth" / "signin" => {
-      req.body.asString.map(body =>
-        body.fromJson[UserReq] match {
-          case Left(e) => Response.status(Status.Forbidden)
-          case Right(user) => Response.json(TokenReq(user.name + "|" + user.password).toJson)
-        }
-      ).orElseFail(Response.status(Status.BadRequest))
-    }
-
-    case req@Method.POST -> !! / "auth" / "signup" => {
-      req.body.asString.map(body =>
-        body.fromJson[UserReq] match {
-          case Left(e) => Response.status(Status.Forbidden)
-          case Right(user) => Response.status(Status.Ok)
-        }
-      ).orElseFail(Response.status(Status.BadRequest))
-    }
-  }
-
-}*/
