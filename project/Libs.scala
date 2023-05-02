6d52d059a307e9986845222e4c19a07fdf097fdc
EgorovGA <egorov.ga@minc.tech>
2023-05-02 17:18:26 +0300

    BODY FINALLY OH YEAH

--- project/Libs.scala
+++ project/Libs.scala
@@ -3,6 +3,7 @@ import sbt._
 object V {
   val zio = "2.0.13"
   val zioHttp = "0.0.5"
+  val zioJson = "0.3.0-RC10"
 
   val pureconfig = "0.17.3"
 }
@@ -12,7 +13,8 @@ object Libs {
 
   val zio: List[ModuleID] = List(
     "dev.zio" %% "zio" % V.zio,
-    "dev.zio" %% "zio-http" % V.zioHttp
+    "dev.zio" %% "zio-http" % V.zioHttp,
+    "dev.zio" %% "zio-json" % V.zioJson
   )
 
   val pureconfig: List[ModuleID] = List(
--- src/main/scala/StartApp.scala
+++ src/main/scala/StartApp.scala
@@ -6,8 +6,9 @@ import zio.{Scope, ZIO, ZIOAppArgs, ZIOAppDefault}
 object StartApp extends ZIOAppDefault {
   override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] =
     for {
+      //_ <- HelperMain.run
       //_ <- RoutingMain.run
       _ <- AuthMain.run
-      _ <- HelperMain.run
+      //_ <- HelperMain.run
     } yield ()
 }
