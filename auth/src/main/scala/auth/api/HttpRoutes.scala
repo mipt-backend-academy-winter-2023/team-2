44be4e4b35116da21803e8b2f71ade12f04bc055
EgorovGA <egorov.ga@minc.tech>
2023-05-13 15:46:19 +0300

    http compiles - need sql implementation

--- auth/src/main/scala/auth/api/HttpRoutes.scala
+++ auth/src/main/scala/auth/api/HttpRoutes.scala
@@ -1,18 +1,30 @@
 package auth.api
 
 import auth.repository.UserRepository
-import auth.model.JsonProtocol.userEncoder
+import auth.model.JsonProtocol._
+import auth.model.User
+import io.circe.jawn.decode
 import io.circe.syntax.EncoderOps
-import zio.ZIO
+import zio.CanFail.canFailAmbiguous1
+import zio._
 import zio.http._
-import zio.http.model.Status.{InternalServerError, NotImplemented}
-import zio.http.model.Method
+import zio.http.model._
+import zio.http.model.Status.{BadRequest, Created, InternalServerError, NotImplemented}
 
 object HttpRoutes {
   val app: HttpApp[UserRepository, Response] =
     Http.collectZIO[Request] {
       case req@Method.POST -> !! / "auth" / "signup" =>
-        ZIO.succeed(Response.status(NotImplemented))
+        (for {
+          bodyStr <- req.body.asString
+          user <- ZIO.fromEither(decode[User](bodyStr)).tapError(e => ZIO.logError(e.getMessage))
+          _ <- UserRepository.add(user)
+          _ <- ZIO.logInfo(s"Created new user $user")
+        } yield ()).either.map {
+          case Right(_) => Response.status(Created)
+          case Left(_) => Response.status(BadRequest)
+        }
+//        ZIO.succeed(Response.status(NotImplemented))
 
       case req@Method.POST -> !! / "auth" / "signin" =>
         UserRepository.findAll().runCollect.map(_.toArray).either.map {
