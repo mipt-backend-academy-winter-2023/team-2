2c7177cabb5e43373682332561618407f8a1bf12
EgorovGA <egorov.ga@minc.tech>
2023-05-14 03:36:08 +0300

    almost works; need jwt token

--- auth/src/main/scala/auth/repository/UserRepositoryImpl.scala
+++ auth/src/main/scala/auth/repository/UserRepositoryImpl.scala
@@ -18,7 +18,7 @@ final class UserRepositoryImpl(pool: ConnectionPool) extends PostgresTableDescri
   }
 
   override def add(user: User): ZIO[UserRepository, Throwable, Unit] = {
-    val query =
+    /*val query =
       insertInto(userTable)(username, password)
         .values(
           (
@@ -27,7 +27,7 @@ final class UserRepositoryImpl(pool: ConnectionPool) extends PostgresTableDescri
           )
         )
     ZIO.logInfo(s"Query to insert customer is ${renderInsert(query)}") *>
-      execute(query).provideSomeLayer(driverLayer).unit
+      execute(query).provideSomeLayer(driverLayer).unit*/
     /*findByCredentials(user).runCollect.map(_.toArray).either.map {
       case Right(arr) => arr match {
         case Array() => {
@@ -51,7 +51,24 @@ final class UserRepositoryImpl(pool: ConnectionPool) extends PostgresTableDescri
       case Left(_) => throw new Exception("Uh oh!!")
     }*/
     // ZIO.fail(new Exception("Uh oh!"))
-    
+    findByCredentials(user).runCollect.map(_.toArray).either.flatMap {
+      case Right(arr) => arr match {
+        case Array() => {
+          val query =
+            insertInto(userTable)(username, password)
+              .values(
+                (
+                  user.username,
+                  user.password
+                )
+              )
+            ZIO.logInfo(s"Query to insert user is ${renderInsert(query)}") *>
+              execute(query).provideSomeLayer(driverLayer).unit
+        }
+        case _ => ZIO.fail(new Exception("User exists"))
+      }
+      case Left(_) => ZIO.fail(new Exception("Error"))
+    }
   }
 }
 
