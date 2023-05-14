aa7ce0fbb793c5c30394ec6631b2fac511c9939a
Khrol Ariana <78618741+arncpp@users.noreply.github.com>
2023-05-14 23:50:33 +0300

    done

---
+++ auth/src/main/scala/auth/repository/UserRepository.scala
@@ -0,0 +1,20 @@
+package auth.repository
+
+import auth.model.User
+import zio.{Task, ZIO}
+import zio.stream.ZStream
+
+trait UserRepository {
+  def findUser(user: User): ZStream[Any, Throwable, User]
+
+  def add(user: User): ZIO[UserRepository, Throwable, Unit]
+}
+
+object UserRepository {
+  def findUser(user: User): ZStream[UserRepository, Throwable, User] =
+    ZStream.serviceWithStream[UserRepository](_.findUser(user))
+
+  def add(user: User): ZIO[UserRepository, Throwable, Unit] =
+    ZIO.serviceWithZIO[UserRepository](_.add(user))
+
+}
---
+++ auth/src/main/scala/auth/repository/UserRepositoryImpl.scala
@@ -0,0 +1,51 @@
+package auth.repository
+
+import auth.model.User
+import auth.security.PasswordUtils
+import zio.{ZIO, ZLayer}
+import zio.sql.ConnectionPool
+import zio.stream.ZStream
+
+final class UserRepositoryImpl(pool: ConnectionPool)
+    extends PostgresTableDescription
+    with UserRepository {
+  val driverLayer: ZLayer[Any, Nothing, SqlDriver] =
+    ZLayer.make[SqlDriver](SqlDriver.live, ZLayer.succeed(pool))
+
+  override def findUser(user: User): ZStream[Any, Throwable, User] = {
+    val selectAll = select(username, password)
+      .from(users)
+      .where(
+        username === user.username && password
+          === PasswordUtils.encode(user.password)
+      )
+
+    ZStream.fromZIO(
+      ZIO.logInfo(s"Query to execute findUser is ${renderRead(selectAll)}")
+    ) *> execute(selectAll.to((User.apply _).tupled))
+      .provideSomeLayer(driverLayer)
+  }
+
+  override def add(user: User): ZIO[UserRepository, Throwable, Unit] = {
+    findUser(user).runCollect.map(_.toArray).either.flatMap {
+      case Right(value) =>
+        value match {
+          case Array() =>
+            val query = insertInto(users)(username, password)
+              .values((user.username, PasswordUtils.encode(user.password)))
+
+            ZIO.logInfo(s"Query to execute add is ${renderInsert(query)}") *>
+              execute(query)
+                .provideSomeLayer(driverLayer)
+                .unit
+          case _ => ZIO.fail(new Exception("User already exists"))
+        }
+      case Left(_) => ZIO.fail(new Exception("Adding error"))
+    }
+  }
+}
+
+object UserRepositoryImpl {
+  val live: ZLayer[ConnectionPool, Throwable, UserRepository] =
+    ZLayer.fromFunction(new UserRepositoryImpl(_))
+}
