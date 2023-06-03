04151aa69ae16abebda911860f2836f7391251a1
EgorovGA <egorov.ga@minc.tech>
2023-06-03 04:14:02 +0300

    rev-1-2 forbid two users with same username

--- auth/src/main/scala/auth/repository/UserRepository.scala
+++ auth/src/main/scala/auth/repository/UserRepository.scala
@@ -7,12 +7,17 @@ import zio.stream.ZStream
 trait UserRepository {
   def findByCredentials(user: User): ZStream[UserRepository, Throwable, User]
 
+  def findByUsernameOnly(user: User): ZStream[UserRepository, Throwable, User]
+
   def add(user: User): ZIO[UserRepository, Throwable, Unit]
 }
 
 object UserRepository {
   def findByCredentials(user: User): ZStream[UserRepository, Throwable, User] =
     ZStream.serviceWithStream[UserRepository](_.findByCredentials(user))
+
+  def findByUsernameOnly(user: User): ZStream[UserRepository, Throwable, User] =
+    ZStream.serviceWithStream[UserRepository](_.findByUsernameOnly(user))
 
   def add(user: User): ZIO[UserRepository, Throwable, Unit] =
     ZIO.serviceWithZIO[UserRepository](_.add(user))
