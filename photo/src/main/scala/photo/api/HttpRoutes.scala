1140f5b32e4784accbbccdc20e8ae6482f30c340
EgorovGA <egorov.ga@minc.tech>
2023-09-29 05:59:47 +0300

    wrong attempt data

--- photo/src/main/scala/photo/api/HttpRoutes.scala
+++ photo/src/main/scala/photo/api/HttpRoutes.scala
@@ -1,63 +1,31 @@
 package photo.api
 
-import io.circe.jawn.decode
-import io.circe.generic.codec.DerivedAsObjectCodec.deriveCodec
-import zio.ZIO
 import zio._
-//import zio.console._
-//import zio.nio.file._
-//import zio.codec.Base64
 import zio.http._
-import zio.http.model.Status.{Ok, BadRequest, Forbidden}
+import zio.stream._
+//import zhttp.http._
+import java.nio.file.Paths
 import zio.http.model.{Method, Status}
-import zio.stream.ZStream
-
 
 object HttpRoutes {
   val app: HttpApp[Any, Response] =
-    Http.collectZIO[Request] {
+    Http.collect[Request] {
       case req @ Method.POST -> !! / "add_photo" =>
-        for {
-          // Reading the photo data from the request body
-          body <- req.getBodyAsString
-          photoDataBase64 = body.split(",")(1) // extract base64 string
-          photoData <- ZIO
-            .effect(Base64.decode(photoDataBase64))
-            .mapError(_ => new Exception("Failed to decode photo data"))
-          // Create the folder if it doesn't exist
-          _ <- Files.createDirectories(Path("my_photos"))
-          // Create a unique filename for the photo
-          filename = java.util.UUID.randomUUID().toString
-          // Save the photo to the "my_photos" folder
-          _ <- Files.write(Path(s"my_photos/$filename"), ZStream.fromIterable(photoData))
-        } yield Response.ok
+        req.body
+          .runCollect
+          .flatMap(bytes => savePhoto(bytes))
+          .foldM(
+            error => Response.text(s"Error saving photo: $error"),
+            _ => Response.text("Photo saved successfully!")
+          )
 
-      case _ => ZIO.succeed(Response.notFound)
-/*        (for {
-          fromIdStr <- ZIO
-            .fromOption(
-              req.url.queryParams
-                .get("fromId")
-                .flatMap(_.headOption)
-            )
-            .tapError(_ => ZIO.logError("Provide fromId argument"))
-          fromId = fromIdStr.toInt
-          toIdStr <- ZIO
-            .fromOption(
-              req.url.queryParams
-                .get("toId")
-                .flatMap(_.headOption)
-            )
-            .tapError(_ => ZIO.logError("Provide toId argument"))
-          toId <- ZIO.succeed(toIdStr.toInt)
-          path <- Graph.astar(fromId, toId)
-        } yield (path)).either.map {
-          case Right(route) => {
-            Response.text(route)
-          }
-          case Left(_) =>
-            Response.status(Status.BadRequest)
-        }*/
+      case _ => Response.text("Invalid route!")
+  }
 
-    }
+  private def savePhoto(photoBytes: Chunk[Byte]): ZIO[Console, Throwable, Unit] = {
+    val photoPath = Paths.get("my_photos/fireball.jpg")
+    val fileStream = ZStream.fromChunk(photoBytes).transduce(ZTransducer.fromOutputStream(photoPath))
+    fileStream.run(Sink.drain)
+  }
 }
+
