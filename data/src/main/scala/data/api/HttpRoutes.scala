26b4e16032aca519b9e5c77661157fd3a42c56e6
Anton Belyi <anton.8938575@gmail.com>
2023-10-01 20:19:12 +0300

    implemented upload

--- data/src/main/scala/data/api/HttpRoutes.scala
+++ data/src/main/scala/data/api/HttpRoutes.scala
@@ -8,9 +8,29 @@ import zio.http.html._
 import zio.http.model.Status.{BadRequest, Forbidden, Ok}
 import zio.http.model.{Method, Status}
 
+import java.io.PrintWriter
+import java.nio.file.{Files, Paths}
+
 object HttpRoutes {
   val app: HttpApp[Any, Response] =
     Http.collectZIO[Request] {
+      case request@Method.PUT -> !! / "upload" =>
+        (for {
+          id <- ZIO
+            .fromOption(
+              request.url.queryParams
+                .get("id")
+                .flatMap(_.headOption)
+            )
+            .tapError(_ => ZIO.logError("Provide id argument"))
+          data <- request.body.asString
+          path = Paths.get(s"tmp/photo_$id")
+          _ <- ZIO.attempt(Files.createFile(path))
+          _ <- ZIO.attempt(Files.writeString(path, data))
+        } yield id).either.map {
+          case Right(id) => Response.text(s"Uploaded photo for $id")
+          case Left(_) => Response.status(Status.BadRequest)
+        }
       case req@Method.GET -> !! / "download"  =>
         (for {
           id <- ZIO
@@ -23,12 +43,10 @@ object HttpRoutes {
         } yield id).either.map {
           case Right(id) => {
             val picture: Html =
-              html(
                 body(
                   p(s"Photo of $id"),
                   img(s"tmp/data_$id")
                 )
-              )
             Response.html(picture)
           }
           case Left(_) => Response.status(Status.BadRequest)
